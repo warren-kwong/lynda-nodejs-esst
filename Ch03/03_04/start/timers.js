93b5b3068addc4a96f190711e82904bbc6b950a0
Warren <warren.p.kwong@gmail.com>
2018-09-28 00:23:54 -0700

    setInterval

--- Ch03/03_04/start/timers.js
+++ Ch03/03_04/start/timers.js
@@ -1,6 +1,13 @@
 var waitTime = 3000;
+var currentTime = 0;
+var waitInterval = 500;
 
 console.log('Wait for it');
+
+setInterval(() => {
+  currentTime += waitInterval;
+  console.log(`waiting ${currentTime / 1000} seconds...`);
+}, waitInterval);
 
 setTimeout(() => {
   console.log('done');
