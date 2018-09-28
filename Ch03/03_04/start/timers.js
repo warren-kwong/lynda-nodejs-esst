b8a03683b46f220d7b55e76d4ab29cbfcbc474f9
Warren <warren.p.kwong@gmail.com>
2018-09-28 00:32:29 -0700

    setInterval percent loading console output

--- Ch03/03_04/start/timers.js
+++ Ch03/03_04/start/timers.js
@@ -1,14 +1,25 @@
 var waitTime = 3000;
 var currentTime = 0;
-var waitInterval = 500;
+var waitInterval = 10;
+var percentWaited = 0;
 
-console.log('Wait for it');
+function writeWaitingPercent(p) {
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  process.stdout.write(`waiting ... ${p}%`);
+}
 
-setInterval(() => {
+var interval = setInterval(() => {
   currentTime += waitInterval;
-  console.log(`waiting ${currentTime / 1000} seconds...`);
+  percentWaited = Math.floor((currentTime / waitTime) * 100);
+  writeWaitingPercent(percentWaited);
 }, waitInterval);
 
 setTimeout(() => {
-  console.log('done');
+  clearInterval(interval);
+  writeWaitingPercent(100);
+  console.log('\n\n done \n\n');
 }, waitTime);
+
+process.stdout.write('\n\n');
+writeWaitingPercent(percentWaited);
