369d9e8e2095490b23400b7a01a252d0483fa7fe
Warren <warren.p.kwong@gmail.com>
2018-10-08 16:15:37 -0700

    Child process w/ spawn

---
+++ Ch04/04_06/start/spawn.js
@@ -0,0 +1,16 @@
+const spawn = require('child_process').spawn;
+
+const cp = spawn('node', ['alwaysTalking']);
+
+cp.stdout.on('data', function(data) {
+  console.log(`STDOUT: ${data.toString()}`);
+});
+
+cp.on('close', function() {
+  console.log('Child Process has ended');
+  process.exit();
+});
+
+setTimeout(function() {
+  cp.stdin.write('stop');
+}, 4000);
