2df315ee2cd56cb34d45230c1596cd70bd44c2bc
Warren <warren.p.kwong@gmail.com>
2018-10-09 23:44:05 -0700

    Removing Directories

--- Ch05/05_06/start/directories.js
+++ Ch05/05_06/start/directories.js
@@ -1,5 +1,29 @@
 const fs = require('fs');
 
-fs.renameSync('./assets/logs', './logs');
+// fs.rename is used to move directories
+// *****************************************
+// fs.renameSync('./assets/logs', './logs');
+// console.log('Directory removed.');
 
-console.log('Directory removed.');
+// fs.rmdir is used to remove directory -- but it MUST be empty
+// *****************************************
+// fs.rmdir('./assets', err => {
+//   if (err) {
+//     throw err; // throw err crashes program
+//   }
+//   console.log('Assets Directory Removed');
+// });
+
+// fs.rmdir only removes empty directories.
+// if you have to remove a directory, you must remove all files within
+// *****************************************
+fs.readdirSync('./logs').forEach(fileName => {
+  fs.unlinkSync('./logs/' + fileName);
+});
+
+fs.rmdir('./logs', err => {
+  if (err) {
+    throw err;
+  }
+  console.log('Logs Directory Removed');
+});
