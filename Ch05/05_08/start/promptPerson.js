78618c36abb8984fb20bd4982404929eca1b4e6a
Warren <warren.p.kwong@gmail.com>
2018-10-10 00:18:13 -0700

    Writable file streams

--- Ch05/05_08/start/promptPerson.js
+++ Ch05/05_08/start/promptPerson.js
@@ -1,34 +1,44 @@
 var readline = require('readline');
 var rl = readline.createInterface(process.stdin, process.stdout);
-var fs = require("fs");
+var fs = require('fs');
 
 var realPerson = {
-	name: '',
-	sayings: []
+  name: '',
+  sayings: []
 };
 
+rl.question('What is the name of a real person? ', function(answer) {
+  realPerson.name = answer;
 
-rl.question("What is the name of a real person? ", function(answer) {
+  var stream = fs.createWriteStream(realPerson.name + '.md');
 
-	realPerson.name = answer;
+  stream.write(`${realPerson.name}\n==================\n\n`);
 
-	//
-	//	TODO: Use a Writable Stream
-	//
+  rl.setPrompt(`What would ${realPerson.name} say? `);
 
+  rl.prompt();
 
-		//
-		//TODO: Write to the stream
-		//
-		
-	});
-
+  rl.on('line', function(saying) {
+    if (saying.toLowerCase().trim() === 'exit') {
+      // close the stream
+      stream.close();
+      rl.close();
+    } else {
+      realPerson.sayings.push(saying.trim());
+      stream.write(`* ${saying.trim()} \n`);
+      rl.setPrompt(
+        `What else would ${realPerson.name} say? ('exit' to leave) `
+      );
+      rl.prompt();
+    }
+  });
 });
 
-
 rl.on('close', function() {
-
-	console.log("%s is a real person that says %j", realPerson.name, realPerson.sayings);
-	process.exit();
-
+  console.log(
+    '%s is a real person that says %j',
+    realPerson.name,
+    realPerson.sayings
+  );
+  process.exit();
 });
