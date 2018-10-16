0cf751bbd80dcbbe1c99bfce577ee8d05ec8fa5f
Warren <warren.p.kwong@gmail.com>
2018-10-16 00:48:27 -0700

    Socket.IO - broadcasting messages

--- Ch09/09_02/start/ws.js
+++ Ch09/09_02/start/ws.js
@@ -1,8 +1,17 @@
-var WebSocketServer = require("ws").Server;
+var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({ port: 3000 });
 
-wss.on("connection", function(ws) {
+wss.on('connection', function(ws) {
+  ws.on('message', message => {
+    if (message === 'exit') {
+      ws.close();
+    } else {
+      // wss.clients is an array of all connected clients
+      wss.clients.forEach(client => {
+        client.send(message);
+      });
+    }
+  });
 
-	ws.send("Welcome to cyber chat");
-
+  ws.send('Welcome to cyber chat');
 });
