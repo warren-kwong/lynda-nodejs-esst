c34567e867e9b25ac68cd7a4f29ff972cbfe9d1a
Warren <warren.p.kwong@gmail.com>
2018-10-16 23:37:16 -0700

    Async Mocha testing

--- Ch10/10_02/start/test/tools-spec.js
+++ Ch10/10_02/start/test/tools-spec.js
@@ -1,14 +1,22 @@
-var expect = require("chai").expect;
-var tools = require("../lib/tools");
+var expect = require('chai').expect;
+var tools = require('../lib/tools');
 
-describe("printName()", function() {
+describe('Tools', function() {
+  describe('printName()', function() {
+    it('should print the last name first', function() {
+      var results = tools.printName({ first: 'Alex', last: 'Banks' });
+      expect(results).to.equal('Banks, Alex');
+    });
+  });
 
-	it("should print the last name first", function() {
+  describe('loadWiki()', function() {
+    this.timeout(5000); // sets the amount of time that the function will wait for done() to be invoked -- default is 2000 (2 sec)
 
-		var results = tools.printName({ first: "Alex", last: "Banks"});
-
-		expect(results).to.equal("Banks, Alex");
-
-	});
-
+    it("should load Abraham Lincoln's wikipedia page", function(done) {
+      tools.loadWiki({ first: 'Abraham', last: 'Lincoln' }, function(html) {
+        expect(html).to.be.ok; // checks to make sure 'html' is defined
+        done();
+      });
+    });
+  });
 });
