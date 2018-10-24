59266f22d99891cf1e7e821d47173e30be3e4ee1
Warren <warren.p.kwong@gmail.com>
2018-10-23 23:16:29 -0700

    Advanced testing Sinon spies

--- Ch10/10_05/start/test/order-spec.js
+++ Ch10/10_05/start/test/order-spec.js
@@ -1,28 +1,32 @@
-var expect = require("chai").expect;
-var rewire = require("rewire");
+var expect = require('chai').expect;
+var rewire = require('rewire');
 
-var order = rewire("../lib/order");
+var order = rewire('../lib/order');
 
-describe("Ordering Items", function() {
+var sinon = require('sinon');
 
-	beforeEach(function() {
+describe('Ordering Items', function() {
+  beforeEach(function() {
+    this.testData = [
+      { sku: 'AAA', qty: 10 },
+      { sku: 'BBB', qty: 0 },
+      { sku: 'CCC', qty: 3 }
+    ];
 
-		this.testData = [
-			{sku: "AAA", qty: 10},
-			{sku: "BBB", qty: 0},
-			{sku: "CCC", qty: 3}
-		];
+    this.console = {
+      log: sinon.spy()
+    };
 
-		order.__set__("inventoryData", this.testData);
+    order.__set__('inventoryData', this.testData);
+    order.__set__('console', this.console);
+  });
 
-	});
+  it('order an item when there are enough in stock', function(done) {
+    var _this = this;
 
-	it("order an item when there are enough in stock", function(done) {
-
-		order.orderItem("CCC", 3, function() {
-			done();
-		});
-
-	});
-
+    order.orderItem('CCC', 3, function() {
+      expect(_this.console.log.callCount).to.equal(1);
+      done();
+    });
+  });
 });
